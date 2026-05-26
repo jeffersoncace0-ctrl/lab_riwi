2c71566bb26f63de2e1663fb8c6e17e0b474e131
Cacerez Jefferson <jeffersoncace0@gmail.com>
2026-05-26 08:00:33 -0500

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-// 1. Declara tus variables aquí
-const nombre = "jefferson";
-let edad = 22;
-let esEstudiante = true;
-// Imprímelas
-console.log(nombre);
-console.log(edad);
-console.log(esEstudiante);
